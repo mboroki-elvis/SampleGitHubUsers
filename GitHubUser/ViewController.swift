efd69945b358961baf63ffaedc1b1e7eb5fccd36
Elvis Mwenda <info@wuni.io>
2022-05-15 11:58:21 +0300

    Initial Commit

---
+++ GitHubUser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitHubUser
+//
+//  Created by Elvis Mwenda on 15/05/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
